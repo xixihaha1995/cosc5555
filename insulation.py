8229b799ce64fb39654a355a0a20c232c5638c62
lichenFrom HP <wulicheneason@gmail.com>
2021-02-01 09:59:45 -0700

    physical mechanism

---
+++ insulation.py
@@ -0,0 +1,21 @@
+# Task
+# T_summer vs tauClo
+# T_winter vs rhoClo
+
+# constants
+# degree C
+TBody = 34
+TSummer = [x for x in range(20,27)]
+TWinter = [x for x in range(12,20)]
+# watts/K/m2
+qBar = 58.2
+
+epsilonSkin = 0.98
+epsilonEnv = 1
+FskToCl = 1
+FclToSk = 1
+
+AreaBody = 2
+AreaEnv = 10
+FEnvToCl = 0.28
+FclToEnv = 0.72
