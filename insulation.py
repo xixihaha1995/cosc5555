10d0d2dd216e6675e7258883769b55a143d4cbf6
lichenFrom HP <wulicheneason@gmail.com>
2021-02-12 20:16:23 -0700

    T2 can be verifed

--- insulation.py
+++ insulation.py
@@ -58,7 +58,14 @@ FclToSk = 1
 # enclosed Air
 # W*(m^-2)*(K^-1)
 kAir = 0.024
+# kClo will be highly dependent on NU(porous media)
 kClo = 0.047
+
+#outside clo hConv should be independent on cloth material
+# W*(m^-2)*(K^-1)
+hConv = 3
+
+
 # m
 thickAir = 2*10**(-3)
 thickClo = 0.5*10**(-3)
@@ -69,94 +76,161 @@ qRS = sigma * TBody ** 4
 X,Y,Z = [],[],[]
 Epsilon = []
 
-for TEnv in range(TEnvLow,TEnvHigh):
-    tauCloList = []
-    rhoCloList = []
-    hConvCEList = []
-    epsilonCloList = []
-    qRE = sigma * TEnv ** 4
+# for TEnv, how can smart clothing adopt to it:
+# by rhoClo, or hConvClo?yy
+#T1
+#While(l1NORM> 1e-7)
+#     T2'
+#     T3
+#     T2 = realT2(T2',T3,T1)
+#     L1NORM = T2 - T2'
+#plot (rhoClo,hConvClo)
 
-    for tauClo in np.linspace(0, 1, 101):
-        # tauClo = 0.03
-        for rhoClo in np.linspace(0, 1, 101):
-            # rhoClo = 0.3
-            if (tauClo + rhoClo) > 1:
-                continue
-            epsilonClo = 1 - tauClo - rhoClo
-            if(epsilonClo<0 or epsilonClo > 1):
-                continue
-            A = -sigma*epsilonClo
-            B = 0
-            C = 0
-            D = -1*kAir/thickAir
-            # E = kAir/thickAir*(TBody)-tauClo*qRE + (epsilonClo - rhoClo) * qRS - Metab
-            E = kAir / thickAir * (TBody) - tauClo * qRE + (
-                        1 - rhoClo) * qRS - Metab
-            p = np.array([[A, B, C, D, E]])
+for TEnv in range(TEnvLow,TEnvHigh):
+    # tauCloList = []
+    # rhoCloList = []
+    # hConvCEList = []
+    # epsilonCloList = []
+    qRE = sigma * TEnv ** 4
+    L1NORM = 0.6
+    convergence = 1e-4
+    for rhoClo in np.linspace(0, 1, 101):
+        # rhoClo = 0.3
+        epsilonClo = 1 - rhoClo
+
+        A1 = sigma * epsilonClo
+        B1 = 0
+        C1 = 0
+        D1 = kAir / thickAir
+        E1 = Metab- kAir / thickAir * (TBody) - (
+                1 - rhoClo) * qRS
+        p1 = np.array([[A1, B1, C1, D1, E1]])
+        try:
+            TCloOneRoots = fqs.quartic_roots(p1)
+        except ZeroDivisionError:
+            continue
+        # print("TCloOne:" + str(TCloOneRoots[0][1].real)+", TEnv:" + str(TEnv))
+        if (TCloOneRoots[0][1].real > 0 and TCloOneRoots[0][
+            1].real > TEnv):
+            TCloOne = TCloOneRoots[0][1].real
+        else:
+            continue
+        TCloTwo = TCloOne - L1NORM
+        while(L1NORM > convergence):
+            qRClOne = epsilonClo*sigma*TCloOne**4
+            qRClTwo = epsilonClo * sigma * TCloTwo ** 4
+
+            A3 = (1-rhoClo)*sigma
+            B3 = 0
+            C3 = 0
+            D3 = hConv
+            E3 = -hConv*TCloTwo + kAir/thickAir*(TBody - TCloOne) + (1-rhoClo)*(qRS) - -qRClOne- qRClTwo
+            p3 = np.array([[A3, B3, C3, D3, E3]])
             try:
-                TCloOneRoots = fqs.quartic_roots(p)
+                T3Roots = fqs.quartic_roots(p3)
             except ZeroDivisionError:
                 continue
-            # print("TCloOne:" + str(TCloOneRoots[0][1].real)+", TEnv:" + str(TEnv))
-            if (TCloOneRoots[0][1].real > 0 and TCloOneRoots[0][1].real > TEnv):
-                TCloOne = TCloOneRoots[0][1].real
+            if (T3Roots[0][1].real > 0):
+                T3 = T3Roots[0][1].real
             else:
                 continue
-            # TCloTwo = TCloOne - Metab/kClo*thickClo
-            TCloTwo = TCloOne
-            # print("TCloOne:"+str(TCloOne)+", TEnv:"+ str(TEnv))
-            qRCloTwo = epsilonClo *sigma*TCloTwo**4
-            qConvCE = Metab - tauClo*qRS+(epsilonClo-rhoClo)*qRE - qRCloTwo
-            hConvCE = qConvCE/(TCloTwo - TEnv)
-            if(hConvCE <= 0 or hConvCE > 50  ):
-                continue
-            tauCloList.append(tauClo)
-            rhoCloList.append(rhoClo)
-            hConvCEList.append(hConvCE)
-            epsilonCloList.append(epsilonClo)
-
-    xtau = np.array(tauCloList)
-    yrho = np.array(rhoCloList)
-    zhconv = np.array(hConvCEList)
-    epcl = np.array(epsilonCloList)
-    X.append(xtau)
-    Y.append(yrho)
-    Z.append(zhconv)
-    Epsilon.append(epcl)
-plots = zip(X,Y,Z)
-# for idx,itm in enumerate(X):
-#     print(len(itm))
-# print(X)
-# print(Y)
-# print(Epsilon)
-# print(Z)
+            T2 = thickClo / 2/kClo*(2*epsilonClo*sigma*(TCloOne-L1NORM/2)**4 - epsilonClo*sigma*(TBody**4+T3**4)) - kAir*thickClo/kClo/thickAir*(TBody - TCloOne)+TCloOne
+            L1NORM = abs(T2-TCloTwo)
+            TCloTwo = T2
 
 
-def loop_plot(plots,len):
-    figs = plt.figure(figsize=(30, 30))
-    xyLabel = True
-    for idx, plot in enumerate(plots):
-        ax=figs.add_subplot(1,len,idx+1, projection='3d')
-        try:
-            # ax.plot_trisurf(plot[0],plot[1],plot[2])
-            ax.scatter(plot[0], plot[1], plot[2],marker="o")
-        except RuntimeError:
-            continue
-        ax.set_title("TEnv: "+str(TEnvLow+1+idx)+"K")
-        if(xyLabel):
-            ax.set_xlabel('tau')
-            ax.set_ylabel('rho')
-            ax.set_zlabel('hConv')
-            # plt.xlim([0, 1])
-            # plt.ylim([0, 1])
-            # xyLabel =False
-    return figs
-figs = loop_plot(plots,len(X))
 
 
-figs.suptitle("Smart Clothing "+opereation+" Maps",fontsize=36)
-figs.savefig(opereation+" from "+str(TEnvLow) +"K to "+str(TEnvHigh)+"K.png")
-plt.show()
+
+
+# for TEnv in range(TEnvLow,TEnvHigh):
+#     tauCloList = []
+#     rhoCloList = []
+#     hConvCEList = []
+#     epsilonCloList = []
+#     qRE = sigma * TEnv ** 4
+#
+#     for tauClo in np.linspace(0, 1, 101):
+#         # tauClo = 0.03
+#         for rhoClo in np.linspace(0, 1, 101):
+#             # rhoClo = 0.3
+#             if (tauClo + rhoClo) > 1:
+#                 continue
+#             epsilonClo = 1 - tauClo - rhoClo
+#             if(epsilonClo<0 or epsilonClo > 1):
+#                 continue
+#             A = -sigma*epsilonClo
+#             B = 0
+#             C = 0
+#             D = -1*kAir/thickAir
+#             # E = kAir/thickAir*(TBody)-tauClo*qRE + (epsilonClo - rhoClo) * qRS - Metab
+#             E = kAir / thickAir * (TBody) - tauClo * qRE + (
+#                         1 - rhoClo) * qRS - Metab
+#             p = np.array([[A, B, C, D, E]])
+#             try:
+#                 TCloOneRoots = fqs.quartic_roots(p)
+#             except ZeroDivisionError:
+#                 continue
+#             # print("TCloOne:" + str(TCloOneRoots[0][1].real)+", TEnv:" + str(TEnv))
+#             if (TCloOneRoots[0][1].real > 0 and TCloOneRoots[0][1].real > TEnv):
+#                 TCloOne = TCloOneRoots[0][1].real
+#             else:
+#                 continue
+#             # TCloTwo = TCloOne - Metab/kClo*thickClo
+#             TCloTwo = TCloOne
+#             # print("TCloOne:"+str(TCloOne)+", TEnv:"+ str(TEnv))
+#             qRCloTwo = epsilonClo *sigma*TCloTwo**4
+#             qConvCE = Metab - tauClo*qRS+(epsilonClo-rhoClo)*qRE - qRCloTwo
+#             hConvCE = qConvCE/(TCloTwo - TEnv)
+#             if(hConvCE <= 0 or hConvCE > 50  ):
+#                 continue
+#             tauCloList.append(tauClo)
+#             rhoCloList.append(rhoClo)
+#             hConvCEList.append(hConvCE)
+#             epsilonCloList.append(epsilonClo)
+#
+#     xtau = np.array(tauCloList)
+#     yrho = np.array(rhoCloList)
+#     zhconv = np.array(hConvCEList)
+#     epcl = np.array(epsilonCloList)
+#     X.append(xtau)
+#     Y.append(yrho)
+#     Z.append(zhconv)
+#     Epsilon.append(epcl)
+# plots = zip(X,Y,Z)
+# # for idx,itm in enumerate(X):
+# #     print(len(itm))
+# # print(X)
+# # print(Y)
+# # print(Epsilon)
+# # print(Z)
+#
+#
+# def loop_plot(plots,len):
+#     figs = plt.figure(figsize=(30, 30))
+#     xyLabel = True
+#     for idx, plot in enumerate(plots):
+#         ax=figs.add_subplot(1,len,idx+1, projection='3d')
+#         try:
+#             # ax.plot_trisurf(plot[0],plot[1],plot[2])
+#             ax.scatter(plot[0], plot[1], plot[2],marker="o")
+#         except RuntimeError:
+#             continue
+#         ax.set_title("TEnv: "+str(TEnvLow+1+idx)+"K")
+#         if(xyLabel):
+#             ax.set_xlabel('tau')
+#             ax.set_ylabel('rho')
+#             ax.set_zlabel('hConv')
+#             # plt.xlim([0, 1])
+#             # plt.ylim([0, 1])
+#             # xyLabel =False
+#     return figs
+# figs = loop_plot(plots,len(X))
+#
+#
+# figs.suptitle("Smart Clothing "+opereation+" Maps",fontsize=36)
+# figs.savefig(opereation+" from "+str(TEnvLow) +"K to "+str(TEnvHigh)+"K.png")
+# plt.show()
 
 
 
