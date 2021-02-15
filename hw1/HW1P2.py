3c99100e85b23220989875e79e7dd583f3f5181c
lichenFrom HP <wulicheneason@gmail.com>
2021-02-15 15:41:25 -0700

    got x and y

--- hw1/HW1P2.py
+++ hw1/HW1P2.py
@@ -1,5 +1,12 @@
 import csv
-with open('LR.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(row)
+import numpy as np
+data = []
+with open('LR.csv', 'r') as csvfile:
+    lines =list(csv.reader(csvfile,delimiter=','))
+    for line in lines:
+        data.append([float(i) for i in line])
+dataArr = np.array(data)
+# data[ny,nx]
+# print(dataArr.shape)
+x = dataArr[:,0]
+y = dataArr[:,3]
