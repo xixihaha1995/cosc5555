2f73e15a5fb716e31440f1f37791a4016aa0e8ee
lichenFrom HP <wulicheneason@gmail.com>
2021-02-16 14:00:39 -0700

    time for serious loss solver

---
+++ hw1/HW1P4.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df_train = pd.read_table("crime-train.txt")
+df_test = pd.read_table("crime-test.txt")
+def softThreshold(rho,lamda):
+
+def coorLassoSolver(converg, lamd, responY, inputX)
+print(df_train.iloc[:,0])
