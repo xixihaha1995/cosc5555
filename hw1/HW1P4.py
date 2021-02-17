5b36cc8ec7f922d6ffad4abe6716f99f1ff37dc9
lichenFrom HP <wulicheneason@gmail.com>
2021-02-16 21:01:19 -0700

    rho is a sum over all the observations

--- hw1/HW1P4.py
+++ hw1/HW1P4.py
@@ -15,20 +15,26 @@ def softThreshold(rho,lamd):
         return rho - lamd/2
     return 0
 
-def coorLassoSolver(converg, lamd, responY, hX,*passweight):
+def coorLassoSolver(converg, lamd,Y, hX,*passweight):
     l1norm = 10
     m,n = hX.shape
     inputX = hX/(np.linalg.norm(hX, axis= 0))
+    # xTrain.assign(Name="weight0")
+    # xTrain["weight0"] = [1] * row
     # weight = np.array([i for i in passweight])
+    responY = Y.reshape(-1,1)
     weight = np.asarray(passweight).T
-    yPred =inputX  @ weight
     weightNext = copy.deepcopy(weight)
     while l1norm > converg:
         for j in range(n):
             # print(inputX.shape)
+
             hJ = inputX[:,j].reshape(-1,1)
+            yPred = inputX @ weight
             # print(hJ.shape)
-            rho = np.sum(hJ *(responY - yPred + weight[j]*hJ[j]))
+            # brac = weight[j]*hJ[j] + responY - yPred
+            # rho = np.sum(hJ *brac)
+            rho = np.dot(hJ.T, (responY-yPred+weight[j]*hJ))
             weightNext[j] = softThreshold(rho,lamd)
         diff = np.abs(weight-weightNext)
         l1norm =max(diff)
@@ -38,13 +44,12 @@ def coorLassoSolver(converg, lamd, responY, hX,*passweight):
 
 
 xTrain = df_train.drop("ViolentCrimesPerPop",axis = 1)
-xTrain.assign(Name = "weight0")
-xTrain["weight0"] =[1]*row
+
 # print(xTrain.head())
 yTrain = df_train.iloc[:,0]
 # print(yTrain.head())
 # for different lambda, different weights
-weightInit = np.random.uniform(low=0,high = 1, size=col)
+weightInit = np.random.uniform(low=0,high = 1, size=col-1)
 print(type(weightInit))
 lamd = 600
 # print(xTrain.shape)
