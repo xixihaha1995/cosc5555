fa67a50c9a7e59bfe3e2f76542d9f34195bd6df8
lichenFrom HP <wulicheneason@gmail.com>
2021-02-16 19:13:28 -0700

    pass correct forms into solver

--- hw1/HW1P4.py
+++ hw1/HW1P4.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 df_train = pd.read_table("crime-train.txt")
 df_test = pd.read_table("crime-test.txt")
+row,col = df_train.shape
+# print(type(row))
+# print(row)
 # print(df_train.iloc[:,0])
 # the first column
 def softThreshold(rho,lamd):
@@ -19,10 +22,32 @@ def coorLassoSolver(converg, lamd, responY, hX,weight):
     while l1norm > converg:
         l1norm = 0
         for j in range(n):
+            print(inputX.shape)
             hJ = inputX[:,j].reshape(-1,1)
+            print(hJ.shape)
             rho = hJ @ (responY - yPred + weight[j]*hJ)
+            oldWeight = weight[j]
             weight[j] = softThreshold(rho,lamd)
+            newWeight  = weight[j]
+            if np.abs(oldWeight, newWeight) > l1norm:
+                l1norm = np.abs(oldWeight, newWeight)
     return weight.flatten()
+
+
+xTrain = df_train.drop("ViolentCrimesPerPop",axis = 1)
+xTrain.assign(Name = "weight0")
+xTrain["weight0"] =[1]*row
+# print(xTrain.head())
+yTrain = df_train.iloc[:,0]
+# print(yTrain.head())
+# for different lambda, different weights
+weightInit = np.random.uniform(size=row)
+# print(weightInit.shape)
+lamd = 600
+print(xTrain.shape)
+weight = coorLassoSolver(1e-6,lamd,yTrain.values,xTrain.values,weightInit)
+
+
 
 
 
