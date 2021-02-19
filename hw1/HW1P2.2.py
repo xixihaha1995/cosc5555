081bdcb6cf018625d305bf8a436a7a623f38a1d9
lichenFrom HP <wulicheneason@gmail.com>
2021-02-19 13:26:52 -0700

    p2.2 done

--- hw1/HW1P2.2.py
+++ hw1/HW1P2.2.py
@@ -18,6 +18,14 @@ y = dataArr[:, 3]
 dummColumn = np.ones((row,))
 H = np.column_stack((dummColumn, x))
 # print(H)
+
+weightA = np.matmul(inv(np.matmul(np.transpose(H), H)), np.matmul(np.transpose(H), y))
+yOne = np.matmul(weightA, [1,1, 1, 1])
+yTwo = np.matmul(weightA, [1,2, 0, 4])
+yThree = np.matmul(weightA, [1,3,2,1])
+# print("weightA:%s, yone:%s, ytwo:%s, ythree:%s"%(weightA,yOne,yTwo,yThree))
+# weightA:[ 5.31416717 -2.00371927  0.53256334 -0.26560187], yone:3.577409374298066, ytwo:0.24432117283603283, ythree:0.10253417289227507
+
 def crossValidation(num_folds,H,y):
     X_train_folds = np.array_split(H, num_folds)
     y_train_folds = np.array_split(y, num_folds)
@@ -29,88 +37,15 @@ def crossValidation(num_folds,H,y):
         X_valid_cv = X_train_folds[i]
         y_valid_cv = y_train_folds[i]
 
+        obs= y_valid_cv.shape
+
         weightA = np.matmul(inv(np.matmul(np.transpose(X_train_cv), X_train_cv)), np.matmul(np.transpose(X_train_cv), y_train_cv))
         yCap = [np.matmul(weightA, item) for item in X_valid_cv]
         geneError.append(np.sum((yCap - y_valid_cv) ** 2))
-    generationErrorA = np.sum(geneError) / num_folds
+    generationErrorA = np.sum(geneError) / num_folds/obs
     return generationErrorA
 
 geneErr = crossValidation(5,H,y)
-print(geneErr)
-
-# weightA = np.matmul(inv(np.matmul(np.transpose(H), H)), np.matmul(np.transpose(H), y))
-# yOne = np.matmul(weightA, [1, 1])
-# yTwo = np.matmul(weightA, [1, 2])
-# yThree = np.matmul(weightA, [1, 3])
-# # print("weightA:%s, yone:%s, ytwo:%s, ythree:%s"%(weightA,yOne,yTwo,yThree))
-# # weightA:[ 5.92794892 -2.03833663], yone:3.8896122844108025, ytwo:1.851275650759999, ythree:-0.18706098289080408
-# # partA up
-#
-# # CV for 5 fold
-# geneError = []
-# for i in range(5):
-#     HSlice = H[20 * i:20 * (i + 1), :]
-#     ySlice = y[20 * i:20 * (i + 1)]
-#     yCap = [np.matmul(weightA, item) for item in HSlice]
-#     geneError.append(np.sum((yCap - ySlice) ** 2))
-# generationErrorA = np.sum(geneError) / 5
-# # print(generationErrorA) 39.39972330162378
-# # partB up
-#
-#
-# weight = [weightA]
-# polynomial = [2, 3, 4, 5]
-#
-# for poly in polynomial:
-#     HPoly = H[:]
-#     for p in range(2, poly + 1):
-#         HPoly = np.column_stack((HPoly, x ** p))
-#
-#     weightPoly = np.matmul(inv(np.matmul(np.transpose(HPoly), HPoly)),
-#                            np.matmul(np.transpose(HPoly), y))
-#     weight.append(weightPoly)
-# # print(weight)
-# # [array([ 5.92794892, -2.03833663]), array([ 3.27369294,  1.98367369, -1.00550258]), array([ 3.27636183e+00,  1.97545996e+00, -1.00034312e+00, -8.59910796e-04]), array([ 3.26318505,  2.0444677 , -1.0788899 ,  0.02978751, -0.00383093]), array([ 3.27845696,  1.92128111, -0.85896933, -0.11791391,  0.03781207,
-# #        -0.0041643 ])]
-# validaX = [1,2,3]
-# validYPoly=[[yOne,yTwo,yThree]]
-#
-# for idx,poly in enumerate(polynomial):
-#     yPoly = []
-#     for curX in validaX:
-#         HPoly = []
-#         for p in range(poly+1):
-#             HPoly.append(curX**p)
-#         yPoly.append(np.matmul(weight[idx+1],HPoly))
-#     validYPoly.append(yPoly)
-# # print(len(validYPoly))
-# validYCap = np.array(validYPoly,dtype=list)
-# # print(validYCap)
-# # [[3.8896122844108025 1.851275650759999 -0.18706098289080408]
-# #  [4.251864056472466 3.2190300049772675 0.17519078917085906]
-# #  [4.250618769472329 3.219030004977256 0.17643607617099732]
-# #  [4.254719440378921 3.2135661153484136 0.1805367470836643]
-# #  [4.256502600356072 3.2135661153679393 0.1787535871174839]]
-# # partC up
-#
-#
-# cvGeneErr = [generationErrorA]
-# for idx, poly in enumerate(polynomial):
-#     tempErr = []
-#     HPoly = dummColumn[:]
-#     for p in range(1,poly + 1):
-#         HPoly = np.column_stack((HPoly, x ** p))
-#     HPolyNd = np.array(HPoly)
-#     # print(HPolyNd.shape)
-#     for i in range(5):
-#         HSlice = HPolyNd[20 * i:20 * (i + 1), :]
-#         ySlice = y[20 * i:20 * (i + 1)]
-#         # print(weight[idx+1])
-#         yCap = [np.matmul(weight[idx+1], item) for item in HSlice]
-#         tempErr.append(np.sum((yCap - ySlice) ** 2))
-#     generationError = np.sum(tempErr) / 5
-#     cvGeneErr.append(generationError)
-# # print(cvGeneErr)
-# # [39.39972330162378, 9.476672230736952, 9.476649284085338, 9.476177987732848, 9.475605485228275]
-
+# print(geneErr)
+# [5.55476927]
 
