37ad5358ef14e77fc7a80fff466052dfb181ec52
lichenFrom HP <wulicheneason@gmail.com>
2021-03-23 15:23:30 -0600

    all step in one figure

--- hw3/hw3P4.py
+++ hw3/hw3P4.py
@@ -86,7 +86,7 @@ def sgdLinear(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
 
     return weight,aveLoss,l2Weights,SSEArr
 
-def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY):
+def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
     row,col = fakeOnlineTrainX.shape
     dummColumn = np.ones((row,))
     HBatch = np.column_stack((dummColumn, fakeOnlineTrainX))
@@ -95,6 +95,7 @@ def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY):
     sumLoss = 0
     aveLoss = []
     l2Weights = []
+    SSEArr = []
     for t in range(epoch):
         obser = np.random.randint(row)
         sample =  HBatch[obser]
@@ -102,11 +103,12 @@ def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY):
         # return
         Ind = fakeOnlineTrainY[obser]
         P =1/(1+np.exp(-1*(weight@sample)))
-        if t > 0:
-            thisPredicated = 0 if P < 0.5 else 1
-            thisL2Loss = (Ind - thisPredicated)**2
-            sumLoss += thisL2Loss
+        thisPredicated = 0 if P < 0.5 else 1
+        thisL2Loss = (Ind - thisPredicated) ** 2
+        sumLoss += thisL2Loss
+        if t > 0 and (t%100 == 0):
             aveLoss.append(sumLoss /t)
+            SSEArr.append(predictTest(xTest,weight,yTest))
         # update weights
         for j in range(col+1):
             hjFeature = sample[j]
@@ -116,23 +118,42 @@ def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY):
         thisL2Weights = np.sqrt(np.sum(weight**2))
         l2Weights.append(thisL2Weights)
 
-    return weight
+    return weight,aveLoss,l2Weights,SSEArr
+# for stepSize in [0.8,1e-3,1-6]:
+#     trainedWeightLinear,aveLossLinear, l2WeightsLinear,SSEArrLinear = sgdLinear(100000,0.8,xTrain,yTrain,xTest,yTest)
+#
+# fig, ax = plt.subplots()
+# ax.plot(aveLossLinear)
+# ax.set_ylabel("aveLossLinear")
+# fig2, ax2 = plt.subplots()
+# ax2.plot(l2WeightsLinear)
+# ax2.set_ylabel("l2WeightsLinear")
+# fig3, ax3 = plt.subplots()
+# ax3.plot(SSEArrLinear)
+# ax3.set_ylabel("SSEArrLinear")
+# plt.show()
 
-trainedWeightLinear,aveLossLinear, l2WeightsLinear,SSEArrLinear = sgdLinear(100000,1e-6,xTrain,yTrain,xTest,yTest)
-fig, ax = plt.subplots()
-ax.plot(aveLossLinear)
-ax.set_ylabel("aveLossLinear")
-fig2, ax2 = plt.subplots()
-ax2.plot(l2WeightsLinear)
-ax2.set_ylabel("l2WeightsLinear")
-fig3, ax3 = plt.subplots()
-ax3.plot(SSEArrLinear)
-ax3.set_ylabel("SSEArrLinear")
+WeightLogisticArr , aveLossLogisticArr, l2WeightsLogisticArr, SSEArrLogisticArr = [],[],[],[]
+fig = plt.figure()
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+for stepSize in [0.8,1e-3,1-6]:
+    WeightLogistic, aveLossLogistic, l2WeightsLogistic, SSEArrLogistic = sgaLogistic(100000,stepSize,xTrain,yTrain,xTest,yTest)
+    ax1.plot(aveLossLogistic,label = "eta= "+str(stepSize))
+    ax1.set_ylabel("aveLossLogistic")
+    ax2.plot(l2WeightsLogistic,label = "eta= "+str(stepSize))
+    ax2.set_ylabel("l2WeightsLogistic")
+    ax3.plot(SSEArrLogistic,label = "eta= "+str(stepSize))
+    ax3.set_ylabel("SSEArrLogistic")
+
+    WeightLogisticArr.append(WeightLogistic)
+    aveLossLogisticArr.append(aveLossLogistic)
+    l2WeightsLogisticArr.append(l2WeightsLogistic)
+    SSEArrLogisticArr.append(SSEArrLogistic)
 plt.show()
 
-
-
-# trainedWeightLogistic = sgaLogistic(100000,1e-6,xTrain,yTrain)
 # print(trainedWeightLinear)
 # print(trainedWeightLogistic)
 
