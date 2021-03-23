c8d4e13ec00c41ad2e2579948b86dbc02ef5715d
lichenFrom HP <wulicheneason@gmail.com>
2021-03-23 14:56:38 -0600

    sgd linear plot

--- hw3/hw3P4.py
+++ hw3/hw3P4.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 
 def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
 
@@ -32,8 +33,8 @@ yTest = test_NF[:,-1]
 xTrain = train_MF[:,:-1]
 yTrain = train_MF[:,-1]
 
-print(xTrain)
-print(yTrain)
+# print(xTrain)
+# print(yTrain)
 # print(xTrain.shape)
 # print(yTrain.shape)
 # print(xTest.shape)
@@ -41,40 +42,97 @@ print(yTrain)
 xTrain = (xTrain - np.min(xTrain,axis = 0))/(np.max(xTrain,axis = 0)-np.min(xTrain,axis = 0))
 # print(xTrain)
 # print(np.max(xTrain,axis = 0))
-def sgdLinear(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY):
+
+def predictTest(xTest,weight,yTest):
+    row,col = xTest.shape
+    dummColumn = np.ones((row,))
+    HBatch = np.column_stack((dummColumn, xTest))
+    predict = HBatch@weight
+    prob = 1 / (1 + np.exp(-predict))
+    # print(prob)
+    # print(np.where(prob > 0.5, 1, 0))
+    SSE = np.sum((yTest -  np.where(prob > 0.5, 1, 0))**2)
+    return SSE
+
+def sgdLinear(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
     row,col = fakeOnlineTrainX.shape
     dummColumn = np.ones((row,))
     HBatch = np.column_stack((dummColumn, fakeOnlineTrainX))
     weight = [0 for i in range(col+1)]
+    sumLoss = 0
+    aveLoss = []
+    l2Weights = []
+    SSEArr = []
     for t in range(epoch):
         obser = np.random.randint(row)
         input = HBatch[obser]
         trueY = fakeOnlineTrainY[obser]
         predict = weight@input
 
-        weight = weight - stepSize*(-2*input.T*( trueY-predict ))
+        Probility = 1 / (1 + np.exp(-predict))
+        thisPredicated = 0 if Probility < 0.5 else 1
+        thisL2Loss = (trueY - thisPredicated) ** 2
+        sumLoss += thisL2Loss
 
-    return weight
+        if t >0 and (t%100 == 0):
+            aveLoss.append(sumLoss / t)
+            SSEArr.append(predictTest(xTest,weight,yTest))
+
+        # update weights
+        weight = weight - stepSize*(-2*input.T*( trueY-predict ))
+        thisL2Weights = np.sqrt(np.sum(weight**2))
+        l2Weights.append(thisL2Weights)
+
+
+    return weight,aveLoss,l2Weights,SSEArr
 
 def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY):
     row,col = fakeOnlineTrainX.shape
     dummColumn = np.ones((row,))
     HBatch = np.column_stack((dummColumn, fakeOnlineTrainX))
-    weight = [0 for i in range(col+1)]
+    weight = np.array([0 for i in range(col+1)])
+    # print(weight.shape)
+    sumLoss = 0
+    aveLoss = []
+    l2Weights = []
     for t in range(epoch):
         obser = np.random.randint(row)
         sample =  HBatch[obser]
+        # print(sample.shape)
+        # return
         Ind = fakeOnlineTrainY[obser]
         P =1/(1+np.exp(-1*(weight@sample)))
+        if t > 0:
+            thisPredicated = 0 if P < 0.5 else 1
+            thisL2Loss = (Ind - thisPredicated)**2
+            sumLoss += thisL2Loss
+            aveLoss.append(sumLoss /t)
+        # update weights
         for j in range(col+1):
             hjFeature = sample[j]
             derivative = hjFeature*(Ind - P)
-            weight = weight +  stepSize*derivative
+            weight[j] = weight[j] +  stepSize*derivative
+
+        thisL2Weights = np.sqrt(np.sum(weight**2))
+        l2Weights.append(thisL2Weights)
 
     return weight
 
-trainedWeightLinear = sgdLinear(100,0.8,xTrain,yTrain)
-trainedWeightLogistic = sgaLogistic(100,0.8,xTrain,yTrain)
-print(trainedWeightLinear)
-print(trainedWeightLogistic)
+trainedWeightLinear,aveLossLinear, l2WeightsLinear,SSEArrLinear = sgdLinear(100000,1e-6,xTrain,yTrain,xTest,yTest)
+fig, ax = plt.subplots()
+ax.plot(aveLossLinear)
+ax.set_ylabel("aveLossLinear")
+fig2, ax2 = plt.subplots()
+ax2.plot(l2WeightsLinear)
+ax2.set_ylabel("l2WeightsLinear")
+fig3, ax3 = plt.subplots()
+ax3.plot(SSEArrLinear)
+ax3.set_ylabel("SSEArrLinear")
+plt.show()
+
+
+
+# trainedWeightLogistic = sgaLogistic(100000,1e-6,xTrain,yTrain)
+# print(trainedWeightLinear)
+# print(trainedWeightLogistic)
 
