6a215151c8451b0e4a92e1d18eaa2bce6b47633e
lichenFrom HP <wulicheneason@gmail.com>
2021-03-23 20:16:49 -0600

    update for sgdlinear

--- hw3/hw3P4.py
+++ hw3/hw3P4.py
@@ -47,44 +47,51 @@ def predictTest(xTest,weight,yTest):
     row,col = xTest.shape
     dummColumn = np.ones((row,))
     HBatch = np.column_stack((dummColumn, xTest))
-    predict = HBatch@weight
-    prob = 1 / (1 + np.exp(-predict))
-    # print(prob)
-    # print(np.where(prob > 0.5, 1, 0))
-    SSE = np.sum((yTest -  np.where(prob > 0.5, 1, 0))**2)
+
+    scores = np.dot(HBatch, weight)
+    # print(scores)
+    prediction = logProb(scores)
+    # print(yTest,prediction)
+    SSE = np.sum((yTest - np.where(prediction > 0.5, 1, 0)) ** 2)
     return SSE
 
 def sgdLinear(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
     row,col = fakeOnlineTrainX.shape
     dummColumn = np.ones((row,))
     HBatch = np.column_stack((dummColumn, fakeOnlineTrainX))
-    weight = [0 for i in range(col+1)]
+    yBatch = fakeOnlineTrainY
+    weights = [0 for i in range(col+1)]
     sumLoss = 0
     aveLoss = []
     l2Weights = []
     SSEArr = []
     for t in range(epoch):
         obser = np.random.randint(row)
-        input = HBatch[obser]
+        sample = HBatch[obser]
         trueY = fakeOnlineTrainY[obser]
         predict = weight@input
 
-        Probility = 1 / (1 + np.exp(-predict))
-        thisPredicated = 0 if Probility < 0.5 else 1
+        scores = np.dot(sample, weights)
+        prediction = logProb(scores)
+
+        thisPredicated = 0 if prediction < 0.5 else 1
         thisL2Loss = (trueY - thisPredicated) ** 2
         sumLoss += thisL2Loss
 
-        if t >0 and (t%100 == 0):
+        if t > 0 and (t % 100 == 0):
             aveLoss.append(sumLoss / t)
-            SSEArr.append(predictTest(xTest,weight,yTest))
+            SSEArr.append(predictTest(xTest, weights, yTest))
 
-        # update weights
-        weight = weight - stepSize*(-2*input.T*( trueY-predict ))
-        thisL2Weights = np.sqrt(np.sum(weight**2))
+        errSignal = yBatch[obser] - prediction
+        gradient = np.dot(-2*sample.T, errSignal)
+        # for j in range
+        weights = weights + stepSize * gradient
+
+        thisL2Weights = np.sqrt(np.sum(weights**2))
         l2Weights.append(thisL2Weights)
 
 
-    return weight,aveLoss,l2Weights,SSEArr
+    return weights,aveLoss,l2Weights,SSEArr
 
 def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
     row,col = fakeOnlineTrainX.shape
@@ -101,42 +108,44 @@ def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
     for t in range(epoch):
         obser = np.random.randint(row)
         sample =  HBatch[obser]
+        Ind = yBatch[obser]
         # print(sample.shape)
         # return
-
-        scores = np.dot(sample,weights)
+        scores = np.dot(sample, weights)
         prediction = logProb(scores)
-        errSignal = yBatch[obser] - prediction
+
+        thisPredicated = 0 if prediction < 0.5 else 1
+        thisL2Loss = (Ind - thisPredicated) ** 2
+        sumLoss += thisL2Loss
+
+        if t > 0 and (t%100 == 0):
+            aveLoss.append(sumLoss /t)
+            SSEArr.append(accuray(xTest,weights,yTest,1))
+            AlltimeWeight.append(weights)
+
+
+
+        errSignal =  Ind - prediction
         gradient = np.dot(sample.T, errSignal)
         # for j in range
         weights = weights + stepSize * gradient
 
-        Ind = fakeOnlineTrainY[obser]
-        P =1/(1+np.exp(-1*(weight@sample)))
-        thisPredicated = 0 if P < 0.5 else 1
-        thisL2Loss = (Ind - thisPredicated) ** 2
-        sumLoss += thisL2Loss
-        if t > 0 and (t%100 == 0):
-            aveLoss.append(sumLoss /t)
-            SSEArr.append(predictTest(xTest,weight,yTest))
-            AlltimeWeight.append(weight)
-        # update weights
-        for j in range(col+1):
-            hjFeature = sample[j]
-            derivative = hjFeature*(Ind - P)
-            weight[j] = weight[j] +  stepSize*derivative
-
-        thisL2Weights = np.sqrt(np.sum(weight**2))
+        thisL2Weights = np.sqrt(np.sum(weights ** 2))
         l2Weights.append(thisL2Weights)
+
 
     return AlltimeWeight,aveLoss,l2Weights,SSEArr
 
 def logProb(scores):
     return 1/(1+np.exp(-scores))
-def accuray(HBatch,weights,yTest):
-    # row, col = xTest.shape
-    # dummColumn = np.ones((row,))
-    # HBatch = np.column_stack((dummColumn, xTest))
+
+def accuray(xTest,weights,yTest,Bias):
+    if Bias == 1:
+        row, col = xTest.shape
+        dummColumn = np.ones((row,))
+        HBatch = np.column_stack((dummColumn, xTest))
+    else:
+        HBatch = xTest
     scores = np.dot(HBatch,weights)
     # print(scores)
     prediction = logProb(scores)
@@ -166,47 +175,47 @@ def Logistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
         accArr.append(accuray(HBatch, weights, yBatch))
 
     return weights, accArr
-
-print(xTrain.shape)
-weights,accurace = Logistic(100000,0.025,xTrain,yTrain,xTest,yTest)
-# print(weights)
-# plt.title("Epoch = 100000, eta = 1e-5")
-plt.plot(accurace)
-plt.xlabel("steps")
-plt.ylabel("SSE")
-plt.show()
+# Little Test
+# print(xTrain.shape)
+# weights,accurace = Logistic(100000,0.025,xTrain,yTrain,xTest,yTest)
+# # print(weights)
+# # plt.title("Epoch = 100000, eta = 1e-5")
+# plt.plot(accurace)
+# plt.xlabel("steps")
+# plt.ylabel("SSE")
+# plt.show()
 # for stepSize in [0.8,1e-3,1-6]:
 #     trainedWeightLinear,aveLossLinear, l2WeightsLinear,SSEArrLinear = sgdLinear(100000,0.8,xTrain,yTrain,xTest,yTest)
 
 
-# fig = plt.figure()
-# ax1 = fig.add_subplot(131)
-# ax2 = fig.add_subplot(132)
-# ax3 = fig.add_subplot(133)
-#
-# bestModel = [[],0]
-# minTestSSE = 1e10
-# for stepSize in [0.8,1e-3,1e-5]:
-#     AlltimeWeightLogistic, aveLossLogistic, l2WeightsLogistic, SSEArrLogistic = sgaLogistic(100000,stepSize,xTrain,yTrain,xTest,yTest)
-#
-#     if np.min(SSEArrLogistic) < minTestSSE:
-#         minTestSSE = np.min(SSEArrLogistic)
-#         timp100 = np.argmin(SSEArrLogistic)
-#         bestModel[0] =  AlltimeWeightLogistic[timp100]
-#         bestModel[1] = stepSize
-#
-#     ax1.plot(aveLossLogistic,label = "eta= "+str(stepSize))
-#     ax2.plot(l2WeightsLogistic,label = "eta= "+str(stepSize))
-#     ax3.plot(SSEArrLogistic,label = "eta= "+str(stepSize))
-#
-#     ax1.set_ylabel("aveLossLogistic")
-#     ax2.set_ylabel("l2WeightsLogistic")
-#     ax3.set_ylabel("SSEArrLogistic")
-#     legend1 = ax1.legend(fontsize='x-large')
-#     legend2 = ax2.legend(fontsize='x-large')
-#     legend3 = ax3.legend(fontsize='x-large')
-#
-#
+fig = plt.figure()
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+bestModel = [[],0]
+minTestSSE = 1e10
+for stepSize in [0.8,1e-3,1e-5]:
+    AlltimeWeightLogistic, aveLossLogistic, l2WeightsLogistic, SSEArrLogistic = sgaLogistic(100000,stepSize,xTrain,yTrain,xTest,yTest)
+
+    if np.min(SSEArrLogistic) < minTestSSE:
+        minTestSSE = np.min(aveLossLogistic)
+        timp100 = np.argmin(aveLossLogistic)
+        bestModel[0] =  AlltimeWeightLogistic[timp100]
+        bestModel[1] = stepSize
+
+    ax1.plot(aveLossLogistic,label = "eta= "+str(stepSize))
+    ax2.plot(l2WeightsLogistic,label = "eta= "+str(stepSize))
+    ax3.plot(SSEArrLogistic,label = "eta= "+str(stepSize))
+
+    ax1.set_ylabel("aveLossLogistic")
+    ax2.set_ylabel("l2WeightsLogistic")
+    ax3.set_ylabel("SSELogistic")
+    legend1 = ax1.legend(fontsize='x-large')
+    legend2 = ax2.legend(fontsize='x-large')
+    legend3 = ax3.legend(fontsize='x-large')
+
+
 # fig2 = plt.figure()
 # ax21 = fig2.add_subplot(131)
 # ax22 = fig2.add_subplot(132)
@@ -226,6 +235,6 @@ plt.show()
 #     legend22 = ax22.legend(fontsize='x-large')
 #     legend23 = ax23.legend(fontsize='x-large')
 #
-# plt.show()
+plt.show()
 
 
