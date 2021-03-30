1f1a2f5c5e218ddf2ba877072a5576c937433e6b
lichenFrom HP <wulicheneason@gmail.com>
2021-03-29 20:08:53 -0600

    the figures looks ugly

--- proposal/bank.py
+++ proposal/bank.py
@@ -1,13 +1,150 @@
 import pandas as pd
 import numpy as np
-import copy
+import matplotlib.pyplot as plt
+import math
+
+
+
+def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
+
+    if random_state is None:
+        random_state = np.random
+
+    exam,fea = x_all_LF.shape
+    N = math.ceil(frac_test*exam)
+
+    # np.random.RandomState(random_state)
+    temp = random_state.permutation(x_all_LF)
+    x_test_NF = temp[0:N,:]
+    x_train_MF = temp[N:,:]
+    return x_train_MF, x_test_NF
+def oneHotEnc(bank):
+    for column in bank:
+        if column == 'y':
+            temp = bank.y.astype('category').cat.codes
+            print(type(temp))
+        else:
+            if bank[column].dtypes == object:
+                temp = pd.get_dummies(bank[column], prefix=column)
+            else:
+                temp =  bank[column]
+        try:
+            # s.append(temp)
+            s = pd.concat([s, temp], axis=1)
+        except NameError:
+            s = pd.DataFrame(data=temp)
+    return s
+
+def logProb(scores):
+    return 1/(1+np.exp(-scores))
+
+def accuray(xTest,weights,yTest,Bias):
+    if Bias == 1:
+        row, col = xTest.shape
+        dummColumn = np.ones((row,))
+        HBatch = np.column_stack((dummColumn, xTest))
+    else:
+        HBatch = xTest
+    scores = np.dot(HBatch,weights)
+    # print(scores)
+    prediction = logProb(scores)
+    # print(yTest,prediction)
+    SSE = np.sum((yTest - np.where(prediction > 0.5, 1, 0)) ** 2)
+    # print(SSE)
+    return SSE
+
+
+
+def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
+    row,col = fakeOnlineTrainX.shape
+    dummColumn = np.ones((row,))
+    HBatch = np.column_stack((dummColumn, fakeOnlineTrainX))
+    yBatch = fakeOnlineTrainY
+    weights = np.array([0 for i in range(col+1)])
+    AlltimeWeight = []
+    # print(weight.shape)
+    sumLoss = 0
+    aveLoss = []
+    l2Weights = []
+    SSEArr = []
+    for t in range(epoch):
+        obser = np.random.randint(row)
+        sample =  HBatch[obser]
+        Ind = yBatch[obser]
+        # print(sample.shape)
+        # return
+        scores = np.dot(sample, weights)
+        prediction = logProb(scores)
+
+        thisPredicated = 0 if prediction < 0.5 else 1
+        thisL2Loss = (Ind - thisPredicated) ** 2
+        sumLoss += thisL2Loss
+
+        if t > 0 and (t%100 == 0):
+            aveLoss.append(sumLoss /t)
+            SSEArr.append(accuray(xTest,weights,yTest,1))
+            AlltimeWeight.append(weights)
+
+
+
+        errSignal =  Ind - prediction
+        gradient = np.dot(sample.T, errSignal)
+        # for j in range
+        weights = weights + stepSize * gradient
+
+        thisL2Weights = np.sqrt(np.sum(weights ** 2))
+        l2Weights.append(thisL2Weights)
+
+
+    return AlltimeWeight,aveLoss,l2Weights,SSEArr
 
 bank = pd.read_csv("bank.csv",delimiter=';')
+df = oneHotEnc(bank)
+df.rename(columns = {0:'y'}, inplace = True)
 
-for column in bank:
-    if bank[column].dtypes == object:
-        pd.get_dummies(bank[column], prefix=column)
-# print(bank.dtypes)
-# print(bank.columns)
-# print(bank.head())
-# print(bank.shape)
+train_MF, test_NF = split_into_train_and_test(df, frac_test=0.3, random_state=np.random.RandomState(0))
+xTest = test_NF[:,:-1]
+yTest = test_NF[:,-1]
+# print(np.count_nonzero(yTest))
+xTrain = train_MF[:,:-1]
+yTrain = train_MF[:,-1]
+# print(xTrain)
+# print(yTrain)
+# minMax normalization
+xTrain = (xTrain - np.min(xTrain,axis = 0))/(np.max(xTrain,axis = 0)-np.min(xTrain,axis = 0))
+xTest = (xTest - np.min(xTest,axis = 0))/(np.max(xTest,axis = 0)-np.min(xTest,axis = 0))
+# print(xTrain)
+# print(yTrain)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+bestModel = [[],0,0,0]
+minTestSSE = 1e10
+for stepSize in [0.8,1e-3,1e-5]:
+    AlltimeWeightLogistic, aveLossLogistic, l2WeightsLogistic, SSEArrLogistic = sgaLogistic(100000,stepSize,xTrain,yTrain,xTest,yTest)
+
+    if np.min(SSEArrLogistic) < minTestSSE:
+        minTestSSE = np.min(aveLossLogistic)
+        timp100 = np.argmin(aveLossLogistic)
+        bestModel[0] =  AlltimeWeightLogistic[timp100]
+        bestModel[1] = stepSize
+        bestModel[2] = timp100*100
+        bestModel[3] = minTestSSE
+
+    ax1.plot(aveLossLogistic,label = "eta= "+str(stepSize))
+    ax2.plot(l2WeightsLogistic,label = "eta= "+str(stepSize))
+    ax3.plot(SSEArrLogistic,label = "eta= "+str(stepSize))
+
+    ax1.set_ylabel("aveLossLogistic")
+    ax2.set_ylabel("l2WeightsLogistic")
+    ax3.set_ylabel("SSELogistic")
+    legend1 = ax1.legend(fontsize='x-large')
+    legend2 = ax2.legend(fontsize='x-large')
+    legend3 = ax3.legend(fontsize='x-large')
+
+plt.show()
+
+
