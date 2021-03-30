d700a0f6f063cc965e0d07b7872857f16fcccae7
lichenFrom HP <wulicheneason@gmail.com>
2021-03-29 18:44:18 -0600

    one hot encoding categorical

--- proposal/bank.py
+++ proposal/bank.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 import copy
-bankTrain = pd.read_csv("bank.csv",delimiter=';')
-print(bankTrain.columns)
-print(bankTrain.head())
-print(bankTrain.shape)
+
+bank = pd.read_csv("bank.csv",delimiter=';')
+
+for column in bank:
+    if bank[column].dtypes == object:
+        pd.get_dummies(bank[column], prefix=column)
+# print(bank.dtypes)
+# print(bank.columns)
+# print(bank.head())
+# print(bank.shape)
