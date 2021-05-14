221184780944740b9ada89c1ae7612d2c3c8a72a
lichenFrom HP <wulicheneason@gmail.com>
2021-05-13 19:20:40 -0600

    precision for weighted imbalanced data

--- proposal/simpleLogistic.py
+++ proposal/simpleLogistic.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from numpy.random import rand
-from sklearn import metrics
+from sklearn import metrics, svm
+from sklearn.metrics import plot_confusion_matrix, precision_score
 from collections import Counter
 
 
@@ -21,8 +22,8 @@ def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
     x_test_NF = temp[0:N,:]
     x_train_MF = temp[N:,:]
     # print(np.count_nonzero(x_all_LF[:,-1]))
-    print(x_all_LF.shape)
-    print(np.count_nonzero(x_all_LF.iloc[:,-1]) /x_all_LF.shape[0] )
+    # print(x_all_LF.shape)
+    # print(np.count_nonzero(x_all_LF.iloc[:,-1]) /x_all_LF.shape[0] )
 
     # print(x_train_MF)
     return x_train_MF, x_test_NF
@@ -122,9 +123,18 @@ def sgaLogistic(epoch,stepSize,fakeOnlineTrainX,fakeOnlineTrainY,xTest,yTest):
     return AlltimeWeight,aveLoss,l2Weights,SSEArr
 
 class logisticRegression:
-    def fit(self,X,y):
+    def fit(self,X,y, epoch = 2e3, lr = 5e-2):
         loss = []
-        weights = n
+        weights = rand(X.shape[1])
+        N = len(X)
+
+        for _ in range(int(epoch)):
+            y_hat = self.sigmod(np.dot(X,weights))
+            weights += lr*np.dot(X.T, y-y_hat) / N
+            loss.append(self.costFunction(X,y,weights))
+        self.weights = weights
+        self.loss = loss
+
     def sigmod(self,z):
         return 1 / (1+np.e**(-z))
     def costFunction(self,X,y,weights):
@@ -135,6 +145,20 @@ class logisticRegression:
         predict0 = (1-y)*np.log(1-self.sigmod(z))
         return -np.sum(predict0+predict1) / len(X)
 
+    def predict(self,X):
+        z = np.dot(X,self.weights)
+        return [1 if i > 0.5 else 0 for i in self.sigmod(z)]
+
+def imbalanced(data):
+    N = data.index.size
+    dataWithOne = data.loc[data["y"] == 1]
+    multiInt =(N - dataWithOne.index.size) // dataWithOne.index.size
+    for _ in range(multiInt):
+        data = data.append(dataWithOne)
+    print(data.index)
+    print(data.loc[data["y"] == 1].index)
+    return data
+
 
 def main():
     # print("original data")
@@ -143,6 +167,7 @@ def main():
     # print("after oneHotEncoding")
     df = oneHotEnc(bank)
     df.rename(columns={0: 'y'}, inplace=True)
+    # df = imbalanced(df)
     # print(type(df))
     # print(df.head())
 
@@ -150,13 +175,26 @@ def main():
     xTest = test_NF[:, :-1]
     yTest = test_NF[:, -1]
 
-    print(type(xTest))
     # print(np.count_nonzero(yTest))
     xTrain = train_MF[:, :-1]
     yTrain = train_MF[:, -1]
 
     xTrain = (xTrain - np.min(xTrain, axis=0)) / (np.max(xTrain, axis=0) - np.min(xTrain, axis=0))
     xTest = (xTest - np.min(xTest, axis=0)) / (np.max(xTest, axis=0) - np.min(xTest, axis=0))
+
+    customModel = logisticRegression()
+    customModel.fit(xTrain,yTrain,1E3)
+    clf = svm.SVC(random_state=0)
+    clf.fit(xTrain,yTrain)
+    cmClf = metrics.confusion_matrix(yTest, clf.predict(xTest))
+    cmCustom = metrics.confusion_matrix(yTest, customModel.predict(xTest))
+    dispClf =metrics.ConfusionMatrixDisplay(confusion_matrix=cmClf)
+    dispCustom = metrics.ConfusionMatrixDisplay(confusion_matrix=cmCustom)
+    dispClf.plot()
+    # dispCustom.plot()
+
+    precision_score(yTest, clf.predict(xTest), average='weighted')
+    plt.show()
 
 
 if __name__ == "__main__":
